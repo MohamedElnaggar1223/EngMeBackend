cd4f0bfd6c7de8b571b871139544b8b002d7ff58
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-12-19 12:50:37 +0200

    Final

--- allowedOrigins.js
+++ allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:4173', 'http://localhost:3000', 'http://192.168.1.3:3000', 'http://localhost:3002', 'http://localhost:5173']
+const allowedOrigins = ['http://localhost:4173', 'http://localhost:3000', 'http://192.168.1.3:3000', 'http://localhost:3002', 'http://localhost:5173', 'https://eng-me-black.vercel.app']
 
 module.exports = allowedOrigins
