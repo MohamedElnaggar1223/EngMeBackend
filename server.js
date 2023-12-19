12a4ea8db69848f345bf982b49dc26c6541f4de0
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-12-19 12:28:36 +0200

    Final

--- server.js
+++ server.js
@@ -1,94 +1,3 @@
-// const express = require('express');
-// const axios = require('axios');
-// const cors = require('cors')
-// const corsOptions = require('./corsOptions')
-
-// const app = express();
-
-// app.use(cors(corsOptions))
-
-// const port = 3001;
-
-// const axiosEndPoint = new axios.Axios()
-
-// app.post('/authorize', async (req, res) => {
-//     const data = await axiosEndPoint.post('https://zoom.us/oauth/authorize', null, {
-//         params: {
-//             'response_type': 200,
-//             'redirect_uri': 'http://localhost:5173',
-//             'client_id': 'FqfeomeMS7aDtXGgWwm3A'
-//         },
-//     })
-
-//     console.log(data.data)
-// })
-
-// const ZOOM_API_KEY = 'YOUR_ZOOM_API_KEY';
-// // const ZOOM_API_SECRET = 'YOUR_ZOOM_API_SECRET';
-
-// // const axiosEndPoints = new axios.Axios()
-
-// // // Endpoint to generate a Zoom meeting link
-// // app.post('/create-meeting', async (req, res) => {
-// //   try {
-// //     const { startTime, endTime, studentId, teacherId } = req.body;
-
-// //     // Use your logic to determine meeting details
-// //     const meetingDetails = {
-// //       topic: 'Consultation Session',
-// //       type: 2, // Scheduled Meeting
-// //       start_time: new Date(startTime).toISOString(),
-// //       duration: Math.floor((endTime - startTime) / 60000), // in minutes
-// //       // Add any other necessary parameters
-// //     };
-
-// //     // Make a request to Zoom API to create a meeting
-// //     const zoomResponse = await axiosEndPoints.post(
-// //       'https://api.zoom.us/v2/users/me/meetings',
-// //       meetingDetails,
-// //       {
-// //         headers: {
-// //           'Content-Type': 'application/json',
-// //           Authorization: Bearer ${await getZoomAccessToken()},
-// //         },
-// //       }
-// //     );
-
-// //     const { join_url, id } = zoomResponse.data;
-
-// //     // Save the Zoom meeting details in your database
-// //     // ... Your database logic here ...
-
-// //     res.json({ joinUrl: join_url, meetingId: id });
-// //   } catch (error) {
-// //     console.error(error);
-// //     res.status(500).json({ error: 'Internal Server Error' });
-// //   }
-// // });
-
-// // // Function to get a Zoom access token
-// // async function getZoomAccessToken() {
-// //   const response = await axios.post(
-// //     'https://zoom.us/oauth/token',
-// //     null,
-// //     {
-// //       params: {
-// //         grant_type: 'client_credentials',
-// //       },
-// //       auth: {
-// //         username: ZOOM_API_KEY,
-// //         password: ZOOM_API_SECRET,
-// //       },
-// //     }
-// //   );
-
-// //   return response.data.access_token;
-// }
-
-// app.listen(port, () => {
-//   console.log(Server is running on port ${port});
-// });
-
 require('dotenv').config()
 const express = require('express');
 const axios = require('axios');
@@ -98,8 +7,31 @@ const corsOptions = require('./corsOptions')
 const querystring = require('querystring')
 const PORT = process.env.PORT || 3001
 
-
 const REDIRECT_URI = 'http://localhost:3001/auth/callback';
+
+var admin = require("firebase-admin");
+
+var serviceAccount = {
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    //@ts-ignore
+    private_key: process.env.private_key.replace(/\\n/g, '\n'),
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain,
+}
+
+admin.initializeApp({
+    //@ts-ignore
+    credential: admin.credential.cert(serviceAccount),
+    projectId: process.env.project_id
+})
+
 
 const session = require('express-session');
 const store = new session.MemoryStore()
@@ -120,12 +52,10 @@ app.use(express.json())
 
 const axiosEndPoint = new axios.Axios()
 
-// let startTimeVar = ''
-// let teacherEmail = ''
-
 app.get('/start-zoom-auth', (req, res) => {
     const startTime = req.query.startTime
     const teacher = req.query.teacher
+    const consultationId = req.query.consultationId
     //@ts-ignore
     if(!req.session?.startTimeVar){
         //@ts-ignore
@@ -133,9 +63,9 @@ app.get('/start-zoom-auth', (req, res) => {
     }
     //@ts-ignore
     req.session.teacherEmail = teacher?.toString() ?? ""
+    //@ts-ignore
+    req.session.consultationId = consultationId?.toString() ?? ""
     res.json({link: `https://zoom.us/oauth/authorize?response_type=code&client_id=aDq5XQyeTFOuObuUlzvenA&redirect_uri=http://localhost:3001/auth/callback`})
-    // console.log(req.session.startTimeVar)
-    // console.log(req.sessionStore)
     res.end()
 })
 
@@ -165,8 +95,6 @@ app.get('/auth/callback', async (req, res) => {
             try
             {
                 const { access_token } = JSON.parse(data.data)
-
-                // console.log(req.session.teacherEmail)
         
                 const newMeeting = {
                     topic: 'Consultation Session',
@@ -180,7 +108,7 @@ app.get('/auth/callback', async (req, res) => {
                     host_email: JSON.parse(Object.values(req.sessionStore.sessions)[0].split(" ")[0]).teacherEmail
                 }
 
-                fetch
+                await fetch
                 (
                     'https://api.zoom.us/v2/users/me/meetings',
                     {
@@ -194,30 +122,20 @@ app.get('/auth/callback', async (req, res) => {
                 )
                 .then(response => response.json())
                 .then(data => {
+                    //@ts-ignore
                     req.sessionStore.set(Object.keys(req.sessionStore.sessions)[0], {...JSON.parse(Object.values(req.sessionStore.sessions)[0].split(" ")[0]), join_url: data.join_url})
-                })
-        
-                // const newAxiosMeetingHeaders = new axios.AxiosHeaders({
-                //     setAuthorization: `Bearer ${access_token}`,
-                //     setContentType: 'application/json',
-                // })
-        
-                // const newAxiosMeeting = new axios.Axios({
-                //     headers: newAxiosMeetingHeaders
-                // })
+                    const db = admin.firestore()
+                    //@ts-ignore
+                    const consultationId = JSON.parse(Object.values(req.sessionStore.sessions)[0].split(" ")[0]).consultationId
+                    const consultationRef = db.collection('consultationSessions').doc(consultationId);
 
-                // console.log(access_token)
-        
-                // const meeting = await newAxiosMeeting.post('https://api.zoom.us/v2/users/me/meetings', newMeeting, 
-                //     {
-                //         headers: {
-                //             'Content-Type': 'application/json',
-                //             Authorization: `Bearer ${access_token}`,
-                //         },
-                //     }
-                // )
-        
-                // console.log(meeting.data)
+                    consultationRef.update({ meetingLink: data.join_url })
+                })
+
+                //@ts-ignore
+                req.session.destroy(() => req.sessionStore.destroy(Object.keys(req.sessionStore.sessions)[0]))
+                res.redirect("http://localhost:5173")
+                res.end()
             }
             catch(e)
             {
@@ -233,145 +151,6 @@ app.get('/auth/callback', async (req, res) => {
     }
 })
 
-app.get('/joinurl', (req, res) => {
-    res.json({ join_url: JSON.parse(Object.values(req.sessionStore.sessions)[0].split(" ")[0]).join_url })
-    // res.redirect('http://localhost:5173')
-})
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-// async function getAccessToken(code)
-// {
-//     axiosEndPoint.post('https://zoom.us/oauth/token', null, {
-//         headers: {
-//             Authorization: `Basic`
-//         }
-//     })
-// }
-
-// app.use(function (req, res, next) {
-    //     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    //     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    //     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-    //     next();
-    // });
-    
-    // app.get('/start-zoom-auth', async (req, res) => {
-        //     try {
-            //         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-            //         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-            //         res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-            //       // Redirect the user to Zoom for authentication
-            //       res.redirect(`https://zoom.us/oauth/authorize?response_type=code&client_id=${ZOOM_CLIENT_ID}&redirect_uri=${REDIRECT_URI}`);
-            //     } catch (error) {
-                //       console.error(error);
-                //       res.status(500).json({ error: 'Internal Server Error' });
-                //     }
-                //   });
-                
-                //   // Callback endpoint to exchange code for an access token
-                //   app.get('/auth/callback', async (req, res) => {
-                    //     const code = req.query.code;
-                    
-                    //     try {
-                        //       // Exchange the code for an access token
-                        //       const response = await axiosEndpoint.post('https://zoom.us/oauth/token', null, {
-                            //         params: {
-                                //           grant_type: 'authorization_code',
-                                //           code,
-                                //           redirect_uri: REDIRECT_URI,
-                                //           client_id: ZOOM_CLIENT_ID,
-                                //           client_secret: ZOOM_CLIENT_SECRET,
-                                //         },
-                                //       });
-                                
-                                //       const accessToken = response.data.access_token;
-                                
-                                //       // Use the access token to create a Zoom meeting
-                                //       const meetingResponse = await axiosEndpoint.post('https://api.zoom.us/v2/users/me/meetings', {
-                                    //         topic: 'Auto-generated Meeting',
-                                    //         type: 2, // Scheduled Meeting
-                                    //       }, {
-                                        //         headers: {
-                                            //           'Content-Type': 'application/json',
-                                            //           Authorization: `Bearer ${accessToken}`,
-                                            //         },
-                                            //       });
-                                            
-                                            //       const joinUrl = meetingResponse.data.join_url;
-                                            
-                                            //       res.json({ joinUrl });
-                                            //     } catch (error) {
-                                                //       console.error(error);
-                                                //       res.status(500).json({ error: 'Internal Server Error' });
-                                                //     }
-                                                //   });
-                                                
-                                                
-                                                // app.get('/zoom-auth', (req, res) => {
-                                                    //     res.redirect(`https://zoom.us/oauth/authorize?response_type=code&client_id=${ZOOM_CLIENT_ID}&redirect_uri=${REDIRECT_URI}`)
-                                                    // })
-                                                    
-                                                    // // Endpoint to generate a Zoom meeting link
-                                                    // app.post('/create-meeting', async (req, res) => {
-                                                        //   try {
-                                                            //     const { startTime, endTime } = req.body;
-                                                            
-                                                            //     const response = await axiosEndpoint.get('https://zoom.us/oauth/authorize', {
-                                                                //         params: {
-                                                                    //             'response_type': 'code',
-                                                                    //             'redirect_uri': 'http://localhost:5173',
-                                                                    //             'client_id': ZOOM_CLIENT_ID
-                                                                    //         }
-                                                                    //     })
-                                                                    
-                                                                    //     console.log(response)
-                                                                    
-                                                                    //     // console.log(new Date(endTime).getTime() , new Date(startTime).getTime())
-                                                                    
-                                                                    //     // Use your logic to determine meeting details
-                                                                    //     const meetingDetails = {
-                                                                        //       topic: 'Consultation Session',
-                                                                        //       type: 2, // Scheduled Meeting
-                                                                        //       start_time: new Date(startTime).toISOString(),
-                                                                        //       duration: Math.floor((new Date(endTime).getTime() - new Date(startTime).getTime()) / 60000), // in minutes
-                                                                        //       // Add any other necessary parameters
-                                                                        //     };
-                                                                        
-                                                                        //     // console.log(meetingDetails)
-                                                                        
-                                                                        //     // Make a request to Zoom API to create a meeting
-                                                                        //     // const zoomResponse = await axiosEndpoint.post(
-                                                                            //     //   'https://api.zoom.us/v2/users/me/meetings',
-                                                                            //     //   meetingDetails,
-                                                                            //     // ).catch(e => console.log(e))
-                                                                            
-                                                                            //     // console.log(zoomResponse)
-                                                                            
-                                                                            //     // const { join_url, id } = zoomResponse.data;
-                                                                            
-                                                                            //     // Save the Zoom meeting details in your database
-                                                                            //     // ... Your database logic here ...
-                                                                            
-                                                                            //     // res.json({ joinUrl: join_url, meetingId: id });
-                                                                            //   } catch (error) {
-                                                                                //     console.error(error);
-                                                                                //     res.status(500).json({ error: 'Internal Server Error' });
-                                                                                //   }
-                                                                                // });
-                                                                                
-                                                                                // // Function to get a Zoom access token using Zoom App credentials
-                                                                                // async function getZoomAccessToken() {
-                                                                                    //   const response = await axiosEndpoint.post(
-                                                                                        //     'https://zoom.us/oauth/token'
-                                                                                        //   );
-                                                                                        
-                                                                                        //   console.log(response.data)
-                                                                                        
-                                                                                        //   return response.data.access_token ?? "";
-                                                                                        // }
-                                                                                        
-                                                                                        // app.listen(port, () => {
-                                                                                            //   console.log(`Server is running on port ${port}`);
-                                                                                            // });
